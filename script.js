2725891bb96b3f34c9542eb2449225b86603028b
Remco van de Siepkamp <69791872+grrremco@users.noreply.github.com>
2021-02-08 17:51:16 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+//Deel 1
+const age = 42;
+
+if (age == 18) {
+    document.write ("U bent 18 en mag naar binnen");
+} else if (age > 18) {
+    document.write ("U bent ouder dan 18 en mag naar binnen")
+} else {
+    document.write ("Je bent nog geen 18 en mag nog niet naar binnen")
+}
+
+//Deel 2
+const imFemale = true;
+
+if (imFemale) {
+    document.write ("U mag naar binnen")
+} else {
+    document.write ("U mag niet naar binnen")
+}
+
+//Deel 3
+const driverStatus = 'BoB';
+
+if (driverStatus == 'BoB') {
+    document.write ('U bent BoB en mag rijden')
+} else {
+    document.write ("U heeft gedronken en mag niet rijden.")
+}
